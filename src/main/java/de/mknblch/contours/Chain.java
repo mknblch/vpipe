93a4ef3d58c7a2ba37b39259c4c39a1fa8b5ed31
Martin Knobloch <mknblch@gmail.com>
2018-02-18 00:52:32 +0100

    WIP

---
+++ src/main/java/de/mknblch/contours/Chain.java
@@ -0,0 +1,18 @@
+package de.mknblch.contours;
+
+/**
+ * @author mknblch
+ */
+public class Chain<I, O> extends Processor<I, O> {
+
+    @Override
+    public O compute(I in) {
+        return in;
+    }
+
+    @Override
+    public <O2> Processor<O, O2> connectTo(Processor<O, O2> nextProcessor) {
+
+
+    }
+}
